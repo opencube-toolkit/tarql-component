703bc2984d9fce454850c3548c6257f38a05cc00
Arkadiusz Stasiewicz <arekstasiewicz@gmail.com>
2014-10-28 11:08:08 +0000

    header row select value factory

---
+++ src/org/deri/opencube/tarql/ui/HeaderrowSelectValueFactory.java
@@ -0,0 +1,18 @@
+package org.deri.opencube.tarql.ui;
+
+import com.fluidops.iwb.ui.configuration.SelectValuesFactory;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public class HeaderrowSelectValueFactory implements SelectValuesFactory {
+	public List<String> getSelectValues() {
+		List<String> choices = Lists.newArrayList();
+
+		choices.add("");
+		choices.add("yes");
+		choices.add("no");
+
+		return choices;
+	}
+}
